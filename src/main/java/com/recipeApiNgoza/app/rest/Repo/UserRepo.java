50a3d4a459538e9cf1cd0682c54c972dbe909a88
Ngoza <nsimuule@gmail.com>
2022-10-28 13:59:11 +0100

    Initial commit

---
+++ src/main/java/com/recipeApiNgoza/app/rest/Repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.recipeApiNgoza.app.rest.Repo;
+
+import com.recipeApiNgoza.app.rest.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+
+
+
+
+}
---
+++ src/test/java/com/recipeApiNgoza/app/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipeApiNgoza.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
